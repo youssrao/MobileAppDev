4e2768114f462a46b4c27e199e64e6101b83405b
Youssra <youssra.outelli@gmail.com>
2020-04-10 19:05:28 +0200

    set initial state for views and variables. Implement rollDice function to take random number from 1 to 6 and set to lastthrow

--- Level-1/Task-1/app/src/main/java/com/youtelli/madlevel1task1/HigherLowerActivity.kt
+++ Level-1/Task-1/app/src/main/java/com/youtelli/madlevel1task1/HigherLowerActivity.kt
@@ -2,11 +2,44 @@ package com.youtelli.madlevel1task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.youtelli.madlevel1task1.databinding.ActivityHigherLowerBinding
 
 class HigherLowerActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHigherLowerBinding
+    private var currentThrow: Int = 1
+    private var lastThrow: Int = 1
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_higher_lower)
+        initViews()
     }
+
+    /**
+     * Set the initial (UI) state of the game.
+     */
+    private fun initViews() {
+        updateUI()
+    }
+
+    /**
+     * Update the last throw text and the dice image resource drawable with the current throw.
+     */
+    private fun updateUI() {
+        binding.textViewLastThrow.text = getString(R.string.last_throw, lastThrow)
+    }
+
+    /**
+     * Replaces the previous dice value with the current one and replaces the current dice with a new dice
+     * with a random number between 1 and 6 (inclusive).
+     */
+    private fun rollDice() {
+        lastThrow = currentThrow
+        currentThrow = (1..6).random()
+        updateUI()
+    }
+
+
 }

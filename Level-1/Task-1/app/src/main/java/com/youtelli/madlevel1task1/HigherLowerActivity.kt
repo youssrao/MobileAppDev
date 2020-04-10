58e36731c548ad7cad271aa8d54f5079b0cbdd12
Youssra <youssra.outelli@gmail.com>
2020-04-10 20:22:39 +0200

    added the functionality for higher, lower and equal. Set button click listeners and updated UI to show dice equal to random number

--- Level-1/Task-1/app/src/main/java/com/youtelli/madlevel1task1/HigherLowerActivity.kt
+++ Level-1/Task-1/app/src/main/java/com/youtelli/madlevel1task1/HigherLowerActivity.kt
@@ -2,7 +2,9 @@ package com.youtelli.madlevel1task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.youtelli.madlevel1task1.databinding.ActivityHigherLowerBinding
+import kotlinx.android.synthetic.main.activity_higher_lower.*
 
 class HigherLowerActivity : AppCompatActivity() {
 
@@ -13,7 +15,8 @@ class HigherLowerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_higher_lower)
+        binding = ActivityHigherLowerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         initViews()
     }
 
@@ -21,6 +24,19 @@ class HigherLowerActivity : AppCompatActivity() {
      * Set the initial (UI) state of the game.
      */
     private fun initViews() {
+        btnEqual.setOnClickListener {
+            onEqualClick()
+        }
+
+        btnHigher.setOnClickListener {
+            onHigherClick()
+        }
+
+        btnLower.setOnClickListener {
+            onLowerClick()
+        }
+
+
         updateUI()
     }
 
@@ -29,16 +45,60 @@ class HigherLowerActivity : AppCompatActivity() {
      */
     private fun updateUI() {
         binding.textViewLastThrow.text = getString(R.string.last_throw, lastThrow)
+
+        when (currentThrow) {
+            1 -> binding.imageViewDice.setImageResource(R.drawable.dice1)
+            2 -> binding.imageViewDice.setImageResource(R.drawable.dice2)
+            3 -> binding.imageViewDice.setImageResource(R.drawable.dice3)
+            4 -> binding.imageViewDice.setImageResource(R.drawable.dice4)
+            5 -> binding.imageViewDice.setImageResource(R.drawable.dice5)
+            6 -> binding.imageViewDice.setImageResource(R.drawable.dice6)
+        }
     }
 
     /**
-     * Replaces the previous dice value with the current one and replaces the current dice with a new dice
-     * with a random number between 1 and 6 (inclusive).
+     * Replaces the previous dice value with the current one and replaces the current dice with a
+     * new dice with a random number between 1 and 6 (inclusive).
      */
     private fun rollDice() {
         lastThrow = currentThrow
         currentThrow = (1..6).random()
         updateUI()
+    }
+
+    private fun onAnswerCorrect() {
+        Toast.makeText(this, getString(R.string.correct), Toast.LENGTH_LONG).show()
+    }
+
+    private fun onAnswerIncorrect() {
+        Toast.makeText(this, getString(R.string.incorrect), Toast.LENGTH_LONG).show()
+    }
+
+    private fun onHigherClick() {
+        rollDice()
+
+        if (currentThrow > lastThrow)
+            onAnswerCorrect()
+        else onAnswerIncorrect()
+
+    }
+
+    private fun onLowerClick() {
+        rollDice()
+
+        if (currentThrow < lastThrow)
+            onAnswerCorrect()
+        else onAnswerIncorrect()
+
+    }
+
+    private fun onEqualClick() {
+        rollDice()
+
+        if (currentThrow == lastThrow)
+            onAnswerCorrect()
+        else onAnswerIncorrect()
+
     }
 
 

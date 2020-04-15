d68a86e63c1442167ded45523e39a4b5118adfe3
Youssra <youssra.outelli@gmail.com>
2020-04-15 23:31:44 +0200

    created data class to store all images and city names. Created cell layout for each item. Create adapter for places to know how and which items to display

---
+++ Level-2/Task-1/PlacesOfInterest/app/src/main/java/com/youtelli/placesofinterest/PlaceAdapter.kt
@@ -0,0 +1,33 @@
+package com.youtelli.placesofinterest
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_place.view.*
+
+public class  PlaceAdapter(private val places: List<Place>) :
+    RecyclerView.Adapter<PlaceAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_place, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return places.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(places[position])
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(place : Place) {
+            itemView.imagePlace.setImageResource(place.imageResId)
+            itemView.textPlace.text = place.name
+        }
+    }
+}

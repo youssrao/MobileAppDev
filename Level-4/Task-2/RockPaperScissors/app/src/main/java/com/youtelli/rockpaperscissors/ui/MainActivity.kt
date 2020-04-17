36ec2122b2e833de8cc2f25bd70bd608c7ca0c90
Youssra <youssra.outelli@gmail.com>
2020-04-17 15:54:51 +0200

    change user image based on choice

--- Level-4/Task-2/RockPaperScissors/app/src/main/java/com/youtelli/rockpaperscissors/ui/MainActivity.kt
+++ Level-4/Task-2/RockPaperScissors/app/src/main/java/com/youtelli/rockpaperscissors/ui/MainActivity.kt
@@ -5,16 +5,48 @@ import android.os.Bundle
 import com.youtelli.rockpaperscissors.R
 import com.youtelli.rockpaperscissors.database.GameRepository
 import com.youtelli.rockpaperscissors.model.Game
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val games = arrayListOf<Game>()
-    private val gameAdapter =
-        GameAdapter(games)
+    private val gameAdapter = GameAdapter(games)
     private lateinit var gameRepository: GameRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initViews()
     }
+
+    private fun initViews() {
+        buttonPaper.setOnClickListener {
+            threwRock()
+        }
+
+        buttonRock.setOnClickListener {
+            threwRock()
+        }
+
+        buttonScissors.setOnClickListener {
+            threwScissors()
+        }
+    }
+
+
+    private fun threwRock() {
+        imageUser.setImageResource(R.drawable.rock)
+    }
+
+    private fun threwScissors() {
+        imageUser.setImageResource(R.drawable.scissors)
+
+    }
+
+    private fun threwPaper() {
+        imageUser.setImageResource(R.drawable.paper)
+
+    }
+
 }
